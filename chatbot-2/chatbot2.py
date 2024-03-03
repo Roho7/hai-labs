aeec2498d6de3c1f59babbd14b01a0b80a954cbc
Roho7 <rohosen2@gmail.com>
2024-03-03 09:16:07 +0000

    Revert "ADDED COSINE"
    This reverts commit de95724e86e30a78f88434136d2c91029009cc64.

---
+++ chatbot-2/chatbot2.py
@@ -0,0 +1,158 @@
+import pandas as pd
+import numpy as np
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import json
+import random
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.pipeline import make_pipeline
+from nltk.stem import WordNetLemmatizer
+from tasks import add_task, show_tasks, remove_tasks, time_task, shift_task, finish_task
+from weather import get_weather
+from name import get_username
+from wiki import get_wiki
+from get_time import get_time, get_day
+
+lemmatizer = WordNetLemmatizer()
+
+
+vocab = json.loads(open("chatbot-2/intents.json").read())
+intents = json.loads(open("chatbot-2/intents.json").read())
+
+stop_words = set(stopwords.words("english"))
+stemmer = PorterStemmer()
+
+
+#  ============== PRE-PROCESSING ============
+def preprocess_text(text):
+    tokens = nltk.word_tokenize(str(text).lower())
+    # tokens = [stemmer.stem(token) for token in tokens]
+    tokens = [
+        lemmatizer.lemmatize(token)
+        for token in tokens
+        if token.isalpha()
+        and token not in stop_words
+        and not token.isdigit()
+        and token not in string.punctuation
+    ]
+    return " ".join(tokens)
+
+
+# ============== USE CLASSIFIER ===============
+X_train = [
+    preprocess_text(pattern)
+    for intent in intents["intents"]
+    for pattern in intent["patterns"]
+]
+y_train = [intent["tag"] for intent in intents["intents"] for _ in intent["patterns"]]
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X_train, y_train, test_size=0.25, random_state=50
+)
+
+classifiers = {
+    "Multinomial Naive Bayes": MultinomialNB(),
+    "Support Vector Machine": SVC(),
+    "Random Forest": RandomForestClassifier(),
+}
+
+kfold = StratifiedKFold(n_splits=7, shuffle=True, random_state=50)
+
+max_score = float("-inf")
+best_classifier = None
+for name, classifier in classifiers.items():
+    print(name)
+    pipeline = make_pipeline(TfidfVectorizer(stop_words="english"), classifier)
+    scores = cross_val_score(pipeline, X_train, y_train, cv=kfold, scoring="accuracy")
+
+    print(f"{name}:")
+    print("Cross-validation scores:", scores)
+    print("Average accuracy:", np.mean(scores))
+    print()
+    if np.mean(scores) > max_score:
+        best_classifier = classifier
+print("best_classifier", best_classifier)
+
+# Creating and training the pipeline
+model = make_pipeline(TfidfVectorizer(), best_classifier)
+model.fit(X_train, y_train)
+
+# Prediction on the test set
+y_pred = model.predict(X_test)
+
+# Evaluation the classifier
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+print(
+    "Classification Report:\n", classification_report(y_test, y_pred, zero_division=1)
+)
+
+
+# ===== GENERATE RESPONSE =====
+def get_response(sentence, raw):
+    predicted_intent = model.predict([pre_processed])[0]
+    intent_responses = {
+        intent["tag"]: intent["responses"] for intent in intents["intents"]
+    }
+    if predicted_intent in intent_responses:
+        responses = intent_responses[predicted_intent]
+        response = random.choice(responses)
+        # print(predicted_intent)
+        if predicted_intent == "add_task":
+            result = add_task(raw)
+            return result
+        if predicted_intent == "show_task":
+            result = show_tasks()
+            return result
+        if predicted_intent == "time_task":
+            result = time_task(raw)
+            return result
+        if predicted_intent == "remove_task":
+            result = remove_tasks(raw)
+            return result
+        if predicted_intent == "shift_task":
+            result = shift_task(raw)
+            return result
+        if predicted_intent == "finish_task":
+            result = finish_task(raw)
+            return result
+        if predicted_intent == "wiki":
+            result = get_wiki(raw)
+            return result
+        if predicted_intent == "weather":
+            result = get_weather(raw)
+            return result
+        if predicted_intent == "username":
+            result = get_username(raw)
+            return result
+        if predicted_intent == "time":
+            result = get_time()
+            return result
+        if predicted_intent == "day":
+            result = get_day()
+            return result
+        return response
+    return None
+
+
+print(
+    "Zeitkonig: Hi! I'm Zeitkonig. I can help you manage your time. You can ask me to add tasks/events.\n\tTo learn more about what I can do, type 'help'."
+)
+while True:
+    user_input = input("You: ")
+    pre_processed = preprocess_text(user_input)
+    response = get_response(pre_processed, user_input)
+    if response != None:
+        response = response
+    else:
+        response = "I do not know how to respond to that. Could you come again?"
+    print(f"Zeitkonig:", response)
---
+++ chatbot-2/tasks.py
@@ -0,0 +1,208 @@
+import nltk
+from cleanup import clean_up_sentence, stem_sentence
+from nltk import pos_tag
+from datetime import datetime
+
+
+tasks = []
+affirmations = [
+    "yes",
+    "ya",
+    "si",
+    "yesh",
+    "yeah",
+    "sure",
+    "right",
+    "correct",
+    "thats right",
+]
+
+
+def fix_time(raw):
+    try:
+        time_pre = datetime.strptime(raw, "%I%p")
+        time = time_pre.strftime("%I:%M %p")
+        return time
+    except:
+        right_time = input(
+            f"Zeitkonig: The time wasn't quite clear, is it {raw}AM or {raw}PM?\nYou: "
+        )
+        if right_time.lower() == "am" or right_time.lower() == "pm":
+            right_time = raw + right_time
+        time_pre = datetime.strptime(right_time, "%I%p")
+        time = time_pre.strftime("%I:%M %p")
+        return time
+
+
+def add_task(sentence):
+    input_list = clean_up_sentence(sentence)
+    task = ""
+    time = ""
+    for item in input_list:
+        if item == "add":
+            task = ""
+        elif item == "at":
+            time_raw = " ".join(input_list[input_list.index("at") + 1 :])
+            time = fix_time(time_raw)
+            break
+        else:
+            task += item + " "
+
+    task = task.strip()
+
+    if time == "":
+        return "Okay, but you need to tell me when you want to do it. Say something like: 'Add [task] at [time]"
+
+    if task and time:
+        for i, item in enumerate(tasks):
+            if time == item["time"]:
+                confirm = input(
+                    f"Zeitkonig: You already have {item['task']} at {time}, are you sure you want to add {task}?\nYou: "
+                )
+                if confirm.lower() in affirmations:
+                    tasks.append({"task": task, "time": time})
+                    return f"Okay {task} added at {time}"
+                else:
+                    return "Okay, try that again"
+        confirm = input(
+            f"Zeitkonig: You want to add {task} at {time}, is that correct?\nYou: "
+        )
+        if confirm.lower() in affirmations:
+            tasks.append({"task": task, "time": time})
+            return f"Okay {task} added at {time}"
+        else:
+            return "Okay, try that again"
+    else:
+        print("Input format is not valid.")
+
+
+def remove_tasks(sentence):
+    if tasks == []:
+        return "You have nothing in your schedule yet."
+    words = nltk.word_tokenize(sentence)
+    tagged = pos_tag(words)
+    task = []
+    for i, (word, pos) in enumerate(tagged):
+        if word in ["cancel", "remove", "delete"]:
+            pass
+        elif word == "from":
+            break
+        else:
+            task.append(word)
+    if task:
+        task = " ".join(task)
+    for item in tasks:
+        print(stem_sentence(item["task"]), stem_sentence(task))
+        if stem_sentence(item["task"]) == stem_sentence(task):
+            tasks.remove(item)
+            other_tasks = show_tasks()
+            return f"{item['task']} removed from your day. {other_tasks}"
+    if task not in tasks:
+        return "Item not found"
+
+
+def sort_tasks():
+    return sorted(tasks, key=lambda x: x["time"])
+
+
+def show_tasks():
+    output = ""
+    tasks = sort_tasks()
+    if not tasks:
+        return "You have nothing scheduled for today"
+    for index, item in enumerate(tasks):
+        if index == 0:
+            message = f"You have", item["task"], "at", item["time"]
+            filtered = " ".join(message)
+            output += filtered
+        elif index == len(tasks) - 1 and len(tasks) > 3:
+            message = f"and finally", item["task"], "at", item["time"]
+            filtered = ", " + " ".join(message)
+            output += filtered
+        else:
+            message = f"and then", item["task"], "at", item["time"]
+            filtered = ", " + " ".join(message)
+            output += filtered
+    return output
+
+
+def time_task(raw):
+    words = nltk.word_tokenize(raw)
+    tagged = pos_tag(words)
+    time = [word for word, tag in tagged if tag == "CD"]
+    for index, item in enumerate(tasks):
+        if fix_time(time[0]) == item["time"]:
+            return f"You have {item['task']} at {time[0]}"
+
+    return f"You don't have anything scheduled at {time[0]}"
+
+
+def shift_task(raw):
+    words = nltk.word_tokenize(raw)
+    tagged = pos_tag(words)
+    time = ""
+    task = []
+    for i, (word, pos) in enumerate(tagged):
+        if pos == "NN":
+            pass
+        elif pos == "CD":
+            time = word
+            break
+        elif word == "to" and i == len(tagged) - 2:
+            pass
+        else:
+            task.append(word)
+    if task:
+        task = " ".join(task)
+
+    # CHECK IF THERE IS ANOTHER TASK AT THE SAME TIME
+    if fix_time(time) in [task["time"] for task in tasks]:
+        existing_task = "".join(
+            [task["task"] for task in tasks if task["time"] == fix_time(time)]
+        )
+        confirm = input(
+            f"Zeitkonig: You already have {existing_task} at {time}, are you sure you want to add {task}?\nYou: "
+        )
+        if confirm in affirmations:
+            for index, item in enumerate(tasks):
+                if task == item["task"]:
+                    item["time"] = fix_time(time)
+                    return f"Okay, {task} shifted to {item['time']}"
+        else:
+            confirmation = input(
+                f"Zeitkonig: Okay, do you want to add {task} at a different time?\nYou: "
+            )
+            if confirmation in affirmations:
+                time_input = input(
+                    f"Zeitkonig: What time would you like to add it?\nYou: "
+                )
+                try:
+                    for index, item in enumerate(tasks):
+                        if task == item["task"]:
+                            item["time"] = fix_time(time_input)
+                            return f"Okay, {task} shifted to {item['time']}"
+                except:
+                    return f"I'm sorry, that wasn't a valid input, try adding the task again."
+            else:
+                return f"Okay, try adding it again."
+    else:
+        for index, item in enumerate(tasks):
+            if stem_sentence(task) == stem_sentence(item["task"]):
+                confirm = input(
+                    f"Zeitkonig: You want to shift {task} to {time}, is that correct?\nYou: "
+                )
+                if confirm.lower() in affirmations:
+                    item["time"] = fix_time(time)
+                    return f"{task} shifted to {item['time']}"
+            else:
+                pass
+        return f"That task is not in your day."
+
+
+def finish_task(raw):
+    task = input(
+        f"Zeitkonig: Which task do you want to remove?\n {', '.join(t['task'] for t in tasks)}\nYou: "
+    )
+    words = nltk.word_tokenize(raw)
+    tagged = pos_tag(words)
+    task = []
